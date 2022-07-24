534031ae5dcb1ea5a2efe95b4f7994450c0c6fb7
Leonid Haidash <Gaydashleo@gmail.com>
2022-07-24 18:48:06 +0300

    555

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,20 @@
 import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+formEl.addEventListener('input', addInput);
+formEl.addEventListener('submit', addMessage);
+
+const formData = {email, message};
+
+function addInput(event) {
+  const emailEl = email.value;
+  
+  localStorage.setItem("feedback-form-state", emailEl);
+}
+
+function addMessage(event) {
+  const messageEl = message.value;
+  localStorage.setItem("feedback-form-state", messageEl);
+}
 
 
