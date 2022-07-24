3498c5f4b1f95d635d2ab3d6aefeee582093d73b
Leonid Haidash <Gaydashleo@gmail.com>
2022-07-24 14:43:14 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,26 +4,24 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 
-const galleryEl = document.querySelector(".gallery");
+const divEl = document.querySelector(".gallery");
 
 
 function createGalleryImage(items) {
   return items.map(item =>  `<div class="gallery__item">
-      <a class="gallery__link" href='${item.original}'>
-      <img
-        class="gallery__image"
-        src='${item.preview}'
-        data-source='${item.original}'
-        alt='${item.description}'
-      />
-    </a>
+      <a class="gallery__item" href='${item.original}'>
+      <img class="gallery__image" src='${item.preview}' alt='${item.description}' />
+      </a> 
   </div>`)
   .join(' ');
     
 };
+
 const addGalleryImage = createGalleryImage(galleryItems);
 
-galleryEl.insertAdjacentHTML("afterbegin", addGalleryImage);
+divEl.insertAdjacentHTML("afterbegin", addGalleryImage);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 });
 // Change code below this line
 
 console.log(galleryItems);
