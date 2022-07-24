fb0a1f33cb9ed807b6dbc640f00fd57f9c694caf
Leonid Haidash <Gaydashleo@gmail.com>
2022-07-24 16:48:18 +0300

    2 end

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const playerEl = document.querySelector('#vimeo-player');
+
+const player = new Player(playerEl);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+function timeSecond(data) {
+  const currentTime = data.seconds;
+  console.log(currentTime);
+  localStorage.setItem('videoplayer-current-time', currentTime);
+};
+
+const time = throttle(timeSecond, 1000);
+
+player.on('timeupdate', time);
+
+const getTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(getTime).then(function(seconds) {
+  }).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+        break;
+
+        default:
+        break;
+    }
+});
+
 
