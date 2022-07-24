0948cb6bf5b2591043043ba4e76dcbb5c0b2b4f1
Leonid Haidash <Gaydashleo@gmail.com>
2022-07-24 14:37:31 +0300

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const galleryEl = document.querySelector(".gallery");
+
+
+function createGalleryImage(items) {
+  return items.map(item =>  `<div class="gallery__item">
+      <a class="gallery__link" href='${item.original}'>
+      <img
+        class="gallery__image"
+        src='${item.preview}'
+        data-source='${item.original}'
+        alt='${item.description}'
+      />
+    </a>
+  </div>`)
+  .join(' ');
+    
+};
+const addGalleryImage = createGalleryImage(galleryItems);
+
+galleryEl.insertAdjacentHTML("afterbegin", addGalleryImage);
 // Change code below this line
 
 console.log(galleryItems);
